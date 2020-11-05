2aee7aedeeecdf0945af66d3caba8dffe16f9fd5
Alberto Fanjul <albertofanjul@gmail.com>
2020-11-05 19:59:56 +0100

    Update to opentk 4

--- Program.cs
+++ Program.cs
@@ -1,84 +1,68 @@
-﻿using System;
-using OpenTK.Input;
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading;
 
 namespace test_joysticks
 {
     class Program
     {
-        static void Main(string[] args)
+        static unsafe void Main(string[] args)
         {
 	     int index = 0; //current device
 	     bool dump = false; //show state and controller name
 	     bool checkold = true; //compare last state 
 	     bool axis = false; //due to bad calibration axis can report data all the time
 
-             JoystickState oldjoystickState = Joystick.GetState(index);
-	     while(true) {
+             NativeWindowSettings nativeSettings = NativeWindowSettings.Default;
+             nativeSettings.Size = new Vector2i(1280, 720);
+             nativeSettings.Title = "Game";
+             nativeSettings.WindowState = WindowState.Normal;
+             nativeSettings.WindowBorder = WindowBorder.Resizable;
+             nativeSettings.NumberOfSamples = 4;
 
-                 JoystickState joystickState = Joystick.GetState(index);
-		 
-                 if (!joystickState.IsConnected) {
-                     Console.WriteLine($"Controller {index} not connected");
-                     for (int i = 0; i < 90; i++) {
-                         if (Joystick.GetState(i).IsConnected) {
-                             Console.WriteLine($"Controller/{i} ({GamePad.GetName(i)})");
-			     index = i;
-	     	             break;
-	     	         }
-                     }
+             GameWindow gameWindow = new GameWindow(GameWindowSettings.Default, nativeSettings);
 
-	             Thread.Sleep(2000);
-	             continue;
-	         }
+	     while (true) {
+                 for (int i = 0; i < gameWindow.JoystickStates.Count; i++)
+                 {
+	             JoystickState state = gameWindow.JoystickStates[i];
+                     if (state != null)
+                     {
+                         GLFW.GetJoystickHatsRaw(i, out var hatCount);
+                         GLFW.GetJoystickAxesRaw(i, out var axisCount);
+                         GLFW.GetJoystickButtonsRaw(i, out var buttonCount);
+                         var name = GLFW.GetJoystickName(i);
 
-                 JoystickCapabilities joystickCapabilities = Joystick.GetCapabilities(index);
+                         Console.WriteLine($"Controller/{i} ({name})");
+                         Console.WriteLine($"{hatCount} {axisCount} {buttonCount}");
+                         for (int j = 0; j < hatCount; j++)
+			 {
+                             Console.WriteLine($"hat{j}: {state.GetHat(j)}");
+			 }
 
-	         if (checkold && joystickState.Equals(oldjoystickState)) {
-	             continue;
-	         }
-
-		 if (dump)
-                     Console.WriteLine($"Controller/{index} ({GamePad.GetName(index)})");
-
-	         oldjoystickState = joystickState;
-		 if (dump) {
-                     Console.WriteLine($"{joystickState.ToString()}");
-	             //Thread.Sleep(2000);
-		 }
-
-                 //Buttons
-                 for (int i = 0; i < joystickCapabilities.ButtonCount; i++) {
-                     if (joystickState.IsButtonDown(i)) {
-                         Console.WriteLine($"button {i}");
-                         Console.WriteLine("pressed");
-                     }
-                 }
-
-		 if (axis) {
-                     //Axis
-                     for (int i = 0; i < joystickCapabilities.AxisCount; i++) {
-                         if (joystickState.GetAxis(i) > 0.5f && joystickState.GetAxis(i) > 0.1f) {
-                             Console.WriteLine($"axis {i}");
-                             Console.WriteLine("move");
-                         }
+                         for (int j = 0; j < axisCount; j++)
+			 {
+                             Console.WriteLine($"axis{j}: {state.GetAxis(j)}");
+			 }
+			     
+                         for (int j = 0; j < buttonCount; j++)
+			 {
+                             if (state.IsButtonDown(j)) {
+                                 Console.WriteLine($"button {j}: down");
+			     }
+			 }
+                         //Console.WriteLine($"Controller/{i} ({state})");
+	                 index = i;
                      }
                  }
-
-                 //Hats
-                 for (int i = 0; i < joystickCapabilities.HatCount; i++) {
-                     JoystickHatState hatState = joystickState.GetHat((JoystickHat)i);
-                     string           pos      = null;
-
-                     if (hatState.IsUp)    pos = "Up";
-                     if (hatState.IsDown)  pos = "Down";
-                     if (hatState.IsLeft)  pos = "Left";
-                     if (hatState.IsRight) pos = "Right";
-                     if (pos == null)      continue;
-
-                     Console.WriteLine($"hat {i}");
-                     Console.WriteLine($"{pos}");
-                 }
+		 //Thread.Sleep(2000);
+		 gameWindow.ProcessEvents();
              }
         }
     }
